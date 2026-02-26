73cc997c09aee91b47283fca239d49572dc1e8c3
Gufran Khan <gufran3004@gmail.com>
2026-02-26 14:30:51 +0530

    installed and configure socket.io

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ require("dotenv").config();
 const app = require("./src/app");
 const PORT = 3000;
 const connectDB = require("./src/db/db");
+const initSocketServer = require("./src/sockets/socket.server");
+const httpServer = require("http").createServer(app);
 
 connectDB();
+initSocketServer(httpServer);
 
 app.get("/", (req, res) => {
   res.json({ msg: "Hi there" });
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server running at port: ${PORT}`);
 });
